d410a1c13e4b07046d00f960646595ae7a10560c
Musab Nasredlin <musabagab2010@gmail.com>
2024-12-22 19:54:45 +0400

    Updated projects

--- source-code/final-project/app/build.gradle.kts
+++ source-code/final-project/app/build.gradle.kts
@@ -6,16 +6,17 @@ plugins {
 	id("kotlin-kapt")
 	id("com.google.dagger.hilt.android")
 	id("com.google.gms.google-services")
+	id("org.jetbrains.kotlin.plugin.compose") version ("2.0.0")
 }
 
 android {
 	namespace = "com.droidcon.authenticate"
-	compileSdk = 33
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "com.droidcon.authenticate"
 		minSdk = 24
-		targetSdk = 33
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0"
 
@@ -72,14 +73,14 @@ dependencies {
 	implementation("com.facebook.android:facebook-android-sdk:16.1.3")
 //	implementation("com.firebaseui:firebase-ui-auth:7.2.0")
 
-	implementation("com.google.dagger:hilt-android:2.46.1")
-	kapt("com.google.dagger:hilt-compiler:2.46.1")
+	implementation("com.google.dagger:hilt-android:2.51.1")
+	kapt("com.google.dagger:hilt-compiler:2.51.1")
 
-	androidTestImplementation("com.google.dagger:hilt-android-testing:2.46.1")
-	kaptAndroidTest("com.google.dagger:hilt-compiler:2.46.1")
+	androidTestImplementation("com.google.dagger:hilt-android-testing:2.51.1")
+	kaptAndroidTest("com.google.dagger:hilt-compiler:2.51.1")
 
-	testImplementation("com.google.dagger:hilt-android-testing:2.46.1")
-	kaptTest("com.google.dagger:hilt-compiler:2.46.1")
+	testImplementation("com.google.dagger:hilt-android-testing:2.51.1")
+	kaptTest("com.google.dagger:hilt-compiler:2.51.1")
 
 	implementation("androidx.navigation:navigation-compose:2.6.0")
 	implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
